04fabde194a2f07eac998c72aab1601a72046305
Tonii2100 <ouanthony9@gmail.com>
2026-04-14 01:32:33 -0700

    Added Unit test for ScoreDAO, fixed spacings, and created ScoreRecord.

--- src/main/java/trivia/ScoreRecord.java
+++ src/main/java/trivia/ScoreRecord.java
@@ -2,14 +2,13 @@ package trivia;
 
 
 /**
- * @Author Anthony Ou
+ * @author Anthony Ou
  * @version 0.1.0
  * @since 4/14/26
  *
  * ScoreRecord Class represents the quiz history record containing a score and the date taken.
  *
  */
-
 
 public class ScoreRecord {
 
--- src/test/java/trivia/ScoreDAOTest.java
+++ src/test/java/trivia/ScoreDAOTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * @author Anthony Ou
  * @version 0.1.0
