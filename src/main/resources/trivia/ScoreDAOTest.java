7bb3aa9b3ac5bb2707224d66efb32ddb03c24f84
Tonii2100 <ouanthony9@gmail.com>
2026-04-13 19:47:55 -0700

    Created ScoreDAOTest.

---
+++ src/main/resources/trivia/ScoreDAOTest.java
@@ -0,0 +1,14 @@
+package trivia;
+
+/**
+ * @author Anthony Ou
+ * @version 0.1.0
+ * @since 4/13/26
+ *
+ * Unit test for the ScoreDAO class
+ *
+ */
+public class ScoreDAOTest{
+
+    private ScoreDAO scoreDAO = new ScoreDAO()
+}
